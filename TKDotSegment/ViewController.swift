ec212195ff3244f74e4b097801323abc111a0057
TBXark <vfanx@qq.com>
2016-04-11 18:17:45 +0800

    Initial Commit

---
+++ TKDotSegment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TKDotSegment
+//
+//  Created by Tbxark on 16/4/11.
+//  Copyright © 2016年 TBXark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
